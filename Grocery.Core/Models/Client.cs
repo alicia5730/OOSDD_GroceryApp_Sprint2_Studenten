4b07eb3f139baa88911ad8afe81270b0636f8126
Alicia <s1183889@student.windesheim.nl>
2025-09-18 17:05:10 +0200

    Login Werkt
    Client class uitgebreid met properties (Email, Password).
    
    ClientRepository aangepast zodat de juiste client op basis van email wordt geretourneerd i.p.v. vaste data.
    
    AuthService verder uitgewerkt → logincontrole via PasswordHelper.VerifyPassword.
    
    LoginView.xaml toegevoegd aan project zodat het meecompileert.
    
    MauiProgram: registraties van AuthService en LoginView geactiveerd.
    
    App.xaml.cs: LoginViewModel beschikbaar gemaakt en LoginView ingesteld als startpagina i.p.v. AppShell.
    
    AppShell.xaml.cs: route naar LoginView geactiveerd.

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -3,12 +3,17 @@ namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string _emailAddress;
-        private string _password { get; set; }
+        public string _emailAddress { get; set; }
+        public string _password { get; set; }
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             _emailAddress=emailAddress;
             _password=password;
+        }
+        public Client() : base(0, string.Empty)
+        {
+            _emailAddress = string.Empty;
+            _password = string.Empty;
         }
     }
 }
