1d85dd464f2d66722117a005781ebffdd5660347
Alicia <s1183889@student.windesheim.nl>
2025-09-18 14:24:44 +0200

    Ik heb hier de GetAvailableProducts aangepast en ook verwerkt met code.

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -35,9 +35,23 @@ namespace Grocery.App.ViewModels
         private void GetAvailableProducts()
         {
             //Maak de lijst AvailableProducts leeg
+            AvailableProducts.Clear();
+
             //Haal de lijst met producten op
+            var products = _productService.GetAll();
+
             //Controleer of het product al op de boodschappenlijst staat, zo niet zet het in de AvailableProducts lijst
-            //Houdt rekening met de voorraad (als die nul is kun je het niet meer aanbieden).            
+            foreach (var product in products)
+            {
+                bool alreadyInList = (from item in MyGroceryListItems where item.ProductId == product.Id select item).Any();
+
+
+                //Houdt rekening met de voorraad (als die nul is kun je het niet meer aanbieden).     
+                if (!alreadyInList && product.Stock > 0)
+                {
+                    AvailableProducts.Add(product);
+                }
+            }
         }
 
         partial void OnGroceryListChanged(GroceryList value)
